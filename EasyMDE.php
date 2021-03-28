a0633d65c4cba7f16165c18e13e34f6c538e5d12
Oleg Balykin <ezoterik.h@gmail.com>
2021-03-28 09:05:43 +0300

    Init

---
+++ EasyMDE.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ezoterik\easymde;
+
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\helpers\Json;
+use yii\web\JsExpression;
+use yii\widgets\InputWidget;
+
+final class EasyMDE extends InputWidget
+{
+    public array $editorOptions = [];
+
+    public function run()
+    {
+        if ($this->hasModel()) {
+            echo Html::activeTextarea($this->model, $this->attribute, $this->options);
+        } else {
+            echo Html::textarea($this->attribute, $this->value, $this->options);
+        }
+
+        $this->registerAssets();
+    }
+
+    protected function registerAssets(): void
+    {
+        $view = $this->getView();
+        Asset::register($view);
+        $varName = Inflector::variablize('editor_' . $this->id);
+        $script = "var {$varName} = new EasyMDE(" . $this->getEditorOptions() . ');';
+        $view->registerJs($script);
+    }
+
+    protected function getEditorOptions(): string
+    {
+        $this->editorOptions['element'] = new JsExpression('document.getElementById("' . $this->options['id'] . '")');
+
+        return Json::encode($this->editorOptions);
+    }
+}
